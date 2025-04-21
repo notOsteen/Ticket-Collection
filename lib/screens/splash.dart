1f35dca6b059f6cc935e1c370f269b37a0c1c98e
Paul Osteen <paulopoyyayil@gmail.com>
2025-04-21 10:58:49 +0530

    added logo and text to splash

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -8,8 +8,10 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
+    var padding = MediaQuery.paddingOf(context);
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      precacheImage(Assets.splashPng.provider(), context);
+      precacheImage(Assets.splash.provider(), context);
 
       Future.delayed(const Duration(seconds: 3), () {
         if (context.mounted) {
@@ -22,8 +24,50 @@ class SplashScreen extends StatelessWidget {
     });
 
     return Scaffold(
-      body: SizedBox.expand(
-        child: Assets.splashPng.image(fit: BoxFit.cover),
+      body: Stack(
+        children: [
+          SizedBox.expand(
+            child: Assets.splash.image(fit: BoxFit.cover),
+          ),
+          Positioned(
+            top: padding.top,
+            left: 0,
+            right: 0,
+            child: Assets.logo.image(
+              fit: BoxFit.contain,
+              height: size.width * 0.32,
+              width: size.width * 0.32,
+            ),
+          ),
+          Positioned(
+            top: padding.top + size.width * 0.32,
+            left: 0,
+            right: 0,
+            child: const Text(
+              'FFJ',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.w900,
+                color: Colors.red,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Positioned(
+            bottom: padding.bottom + size.width * 0.1,
+            left: 0,
+            right: 0,
+            child: const Text(
+              'TICKET CALCULATOR',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.w900,
+                color: Colors.green,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
     );
   }
